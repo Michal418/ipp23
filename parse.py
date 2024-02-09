0550b673a20c64a201de5863034c5013c42b26df
Michal Cenek <michal.cenek.1@gmail.com>
2024-02-09 16:24:54 +0100

    init

---
+++ parse.py
@@ -0,0 +1,226 @@
+from typing import List, Generator, Iterable
+import re
+from sys import stderr, stdout, exit, argv
+import fileinput
+from xml.dom import minidom
+from xml.sax.saxutils import escape
+
+
+class ParseError(Exception):
+    def __init__(self, message: str, code: int):
+        self.code = code
+        super().__init__(message)
+
+
+class SourceError(ParseError):
+    def __init__(self, message: str):
+        super().__init__('Lexikální nebo syntaktická chyba: ' + message, 23)
+
+
+class Argument:
+    def __init__(self, ipptype: str, text: str):
+        self.ipptype = ipptype
+        self.text = text
+
+
+class Instruction:
+    def __init__(self, order: int, opcode: str, *args: Argument):
+        self.order = order
+        self.opcode = opcode
+        self.args = args
+
+        if len(args) > 3:
+            raise RuntimeError('Počet argumentů musí být menší nebo roven 3')
+
+
+def remove_comment(line: str) -> str:
+    idx = line.find('#')
+
+    if idx == -1:
+        return line
+
+    return line[:idx]
+
+
+def parse_variable(variable_str: str) -> Argument:
+    match = re.match('^(LF|TF|GF)@([a-zA-Z_$&%*!?][a-zA-Z0-9_$&%*!?]*)$', variable_str)
+
+    if match is None:
+        raise SourceError(f'Proměnná ve špatném formátu: "{variable_str}"')
+
+    return Argument('var', variable_str)
+
+
+def parse_literal(literal_str: str) -> Argument:
+    match = re.match('^(int|bool|string|nil)@(.*)$', literal_str)
+
+    if match is None or len(match.groups()) != 2:
+        raise SourceError(f'Konstanta ve špatném formátu: "{literal_str}"')
+
+    typestr, value = match.groups()
+
+    if typestr == 'int' and re.match('^-?[0-9]+$', value) is None:
+        raise SourceError(f'Celočíselná konstanta ve špatném formátu: "{literal_str}"')
+    elif typestr == 'bool' and value not in ('true', 'false'):
+        raise SourceError(f'Booleanská konstanta ve špatném formátu: "{literal_str}"')
+    elif typestr == 'string' and re.match(r'^([^ \t\r\n\x00-\x1F#\\]|\\[0-9]{3})*$', value) is None:
+        raise SourceError(f'Řetězcová konstanta ve špatném formátu: "{literal_str}"')
+    elif typestr == 'nil' and value != 'nil':
+        raise SourceError(f'Nil konstanta ve špatném formátu: "{literal_str}"')
+
+    return Argument(typestr, value)
+
+
+def parse_label(label_str: str) -> Argument:
+    if re.match(r'^([^ \t\r\n\x00-\x1F#\\]|\\[0-9]{3})+$', label_str) is None:
+        raise SourceError(f'Návěstí ve špatném formátu: "{label_str}"')
+
+    return Argument('lable', label_str)
+
+
+def is_possibly_literal(literal_str: str) -> bool:
+    return any(literal_str.startswith(t) for t in ['int', 'bool', 'string', 'nil'])
+
+
+def is_possibly_variable(variable_str: str) -> bool:
+    return any(variable_str.startswith(t) for t in ['TF', 'LF', 'GF'])
+
+
+def parse_symbol(symbol_str: str) -> Argument:
+    if is_possibly_literal(symbol_str):
+        return parse_literal(symbol_str)
+    elif is_possibly_variable(symbol_str):
+        return parse_variable(symbol_str)
+    else:
+        raise SourceError(f'Symbol ve špatném formátu: "{symbol_str}"')
+
+
+def parse_type(ipptype_str: str) -> Argument:
+    if ipptype_str not in ('int', 'string', 'bool'):
+        raise SourceError(f'neznámý typ: "{ipptype_str}"')
+
+    return Argument(ipptype_str, ipptype_str)
+
+
+def tokenize_line(line: str) -> List[str]:
+    result = remove_comment(line).strip()
+    result = result.split()
+
+    if len(result) > 0:
+        result[0] = result[0].upper()
+
+    return result
+
+
+def parse_program(lines: List[str]) -> Generator[Instruction, None, None]:
+    if not lines or lines[0] != '.IPPcode24\n':
+        raise ParseError('Chybná nebo chybějící hlavička', 21)
+
+    OPCODES = ('CREATEFRAME', 'PUSHFRAME', 'POPFRAME', 'RETURN', 'BREAK',
+               'CALL', 'LABEL', 'JUMP', 'MOVE', 'INT2CHAR', 'STRLEN', 'TYPE',
+               'MOVE', 'INT2CHAR', 'STRLEN', 'TYPE', 'ADD', 'SUB', 'MUL',
+               'IMUL', 'DIV', 'IDIV', 'LT', 'GT', 'EQ', 'AND', 'OR', 'NOT',
+               'CONCAT', 'GETCHAR', 'SETCHAR', 'STRI2INT', 'PUSHS', 'WRITE',
+               'DPRINT', 'EXIT', 'POPS', 'DEFVAR', 'READ', 'JUMPIFEQ', 'JUMPIFNEQ')
+
+    order = 1
+    for line in lines[1:]:
+        tokens = tokenize_line(line)
+
+        if tokens and tokens[0] not in OPCODES:
+            raise ParseError(f'Neznámý nebo chybný operační kód: "{tokens[0]}"', 22)
+
+        match tokens:
+            case ('CREATEFRAME' | 'PUSHFRAME' | 'POPFRAME' | 'RETURN' | 'BREAK') as opcode,:
+                yield Instruction(order, opcode)
+
+            case ('CALL' | 'LABEL' | 'JUMP') as opcode, label:
+                yield Instruction(order, opcode, parse_label(label))
+
+            case ('MOVE' | 'INT2CHAR' | 'STRLEN' | 'TYPE') as opcode, var, symb:
+                yield Instruction(order, opcode, parse_variable(var), parse_symbol(symb))
+
+            case ('ADD' | 'SUB' | 'MUL' | 'IMUL' | 'DIV' | 'IDIV' | 'LT' | 'GT' | 'EQ' |
+                  'AND' | 'OR' | 'NOT' | 'CONCAT' | 'GETCHAR' | 'SETCHAR' | 'STRI2INT') as opcode, var, symb1, symb2:
+                yield Instruction(order, opcode, parse_variable(var), parse_symbol(symb1), parse_symbol(symb2))
+
+            case ('PUSHS' | 'WRITE' | 'DPRINT' | 'EXIT') as opcode, symb:
+                yield Instruction(order, opcode, parse_symbol(symb))
+
+            case ('POPS' | 'DEFVAR') as opcode, var:
+                yield Instruction(order, 'POPS', parse_variable(var))
+
+            case 'READ', var, ipptype:
+                yield Instruction(order, 'READ', parse_type(ipptype))
+
+            case ('JUMPIFEQ' | 'JUMPIFNEQ') as opcode, label, symb1, symb2:
+                yield Instruction(order, opcode, parse_label(label), parse_symbol(symb1), parse_symbol(symb2))
+
+            case []:
+                continue
+
+            case _:
+                raise SourceError(f'Špatný počet argumentů: "{tokens}"')
+
+        order += 1
+
+
+def instructions_to_xml(instructions: Iterable[Instruction]) -> minidom.Document:
+    doc = minidom.Document()
+
+    root = doc.createElement('program')
+    root.setAttribute('language', 'IPPcode24')
+    doc.appendChild(root)
+
+    for instruction in instructions:
+        elem = doc.createElement('instruction')
+        elem.setAttribute('order', str(instruction.order))
+        elem.setAttribute('opcode', instruction.opcode)
+
+        for arg_idx, arg in enumerate(instruction.args, 1):
+            arg_elem = doc.createElement(f'arg{arg_idx}')
+            arg_elem.setAttribute('type', arg.ipptype)
+            text_node = doc.createTextNode(arg.text)
+            arg_elem.appendChild(text_node)
+            elem.appendChild(arg_elem)
+
+        root.appendChild(elem)
+
+    return doc
+
+
+def main():
+    HELP = 'Načte ze standardního vstupu zdrojový kód v IPPcode24, ' \
+           'zkontroluje lexikální a sytaktickou správnost kódu a ' \
+           'vypíše na standardní výstup XML reprezentaci programu.'
+
+    if len(argv) == 2 and argv[1] == '--help':
+        print(HELP)
+        return
+    elif len(argv) > 2:
+        raise ParseError('Zakázaná kombinace parametrů', 10)
+
+    try:
+        lines = fileinput.input()
+    except Exception as err:
+        raise ParseError(str(err), 11)
+
+    instructions = parse_program(list(lines))
+    document = instructions_to_xml(instructions)
+
+    try:
+        document.writexml(stdout, indent='  ', addindent='  ', newl='\n')
+    except Exception as err:
+        raise ParseError(str(err), 12)
+
+
+try:
+    main()
+except ParseError as err:
+    print(err, file=stderr)
+    exit(err.code)
+except Exception as err:
+    print('Internal error:', err, file=stderr)
+    exit(99)
+else:
+    exit(0)
---
+++ test.py
@@ -0,0 +1,144 @@
+import json
+from pathlib import Path
+import subprocess
+from typing import List, Generator
+
+
+GREEN = '\033[92m' #]
+RED = '\033[91m' #]
+BLUE = '\033[94m' #]
+MAGENTA = '\033[95m' #]
+CYAN = '\033[96m' #]
+RESET = '\033[0m' #]
+
+
+def color(s: str, color: str) -> str:
+    return f'{color}{s}{RESET}'
+
+
+def blue(s: str) -> str:
+    return color(s, BLUE)
+
+
+def red(s: str) -> str:
+    return color(s, RED)
+
+
+def green(s: str) -> str:
+    return color(s, GREEN)
+
+
+def magenta(s: str) -> str:
+    return color(s, MAGENTA)
+
+
+class TestResult:
+    def __init__(self, description: str, input_file: str, expected_code: int, actual_code: int):
+        self.input_file = input_file
+        self.expected_code = expected_code
+        self.actual_code = actual_code
+        self.description = description
+
+    def __str__(self):
+        status = blue('PASSED') if self.expected_code == self.actual_code else f'{red('FAILED')} with {self.actual_code}'
+        return f'{self.description} {self.input_file} {status}'
+
+class Test:
+    def __init__(self, description: str, code: int, show_stdout: bool, show_stderr: bool):
+        self.description = description
+        self.code = code
+        self.show_stdout = show_stdout
+        self.show_stderr = show_stderr
+        self.tests: List[Path] = []
+        self.text_inputs: List[str] = []
+
+    def test(self, program: str, tests_dir: Path) -> Generator[TestResult, None, None]:
+        for test in self.tests:
+            file_name = str(tests_dir / test)
+
+            with open(file_name, 'r') as file:
+                stdout_dev = None if self.show_stdout else subprocess.DEVNULL
+                stderr_dev = None if self.show_stderr else subprocess.DEVNULL
+                p = subprocess.run(['python', program], stdin=file, stdout=stdout_dev, stderr=stderr_dev)
+
+            yield TestResult(self.description, file_name, self.code, p.returncode)
+
+        for text in self.text_inputs:
+            stdout_dev = None if self.show_stdout else subprocess.DEVNULL
+            stderr_dev = None if self.show_stderr else subprocess.DEVNULL
+            p = subprocess.run(['python', program], input=text.encode(), stdout=stdout_dev, stderr=stderr_dev)
+
+            yield TestResult(self.description, '*text*', self.code, p.returncode)
+
+
+class Tester:
+    def __init__(self, program: Path, tests_dir: Path, *tests: Test):
+        self.tests: List[Test] = list(tests)
+        self.program = program
+        self.tests_dir = tests_dir
+        self.passed_count = 0
+        self.failed_count = 0
+
+    def test(self) -> Generator[TestResult, None, None]:
+        for test in self.tests:
+            for result in test.test(str(self.program), self.tests_dir):
+                if result.actual_code == result.expected_code:
+                    self.passed_count += 1
+                else:
+                    self.failed_count += 1
+
+                yield result
+
+    def result(self) -> str:
+        count = sum(len(t.tests) for t in self.tests) + sum(len(t.text_inputs) for t in self.tests)
+        text = f'passed {self.passed_count} / {count}'
+        return green(text) if self.passed_count == count else magenta(text)
+
+
+def main():
+    with open('test.json', 'r') as file:
+        data = json.load(file)
+
+    program = '.' / Path(data['app'])
+    tests_dir = '.' / Path(data['input-directory'])
+
+    tester = Tester(program, tests_dir)
+
+    for test_dict in data['tests']:
+        description = test_dict['description']
+        code = int(test_dict['code'])
+
+        if 'stderr' not in test_dict:
+            show_stderr = True
+        elif test_dict['stderr'] == 'show':
+            show_stderr = True
+        elif test_dict['stderr'] == 'hide':
+            show_stderr = False
+        else:
+            raise ValueError()
+
+        if 'stdout' not in test_dict:
+            show_stdout = False
+        elif test_dict['stdout'] == 'show':
+            show_stdout = True
+        elif test_dict['stdout'] == 'hide':
+            show_stdout = False
+        else:
+            raise ValueError()
+
+        test = Test(description, code, show_stdout, show_stderr)
+
+        if 'files' in test_dict:
+            test.tests.extend(Path(file) for file in test_dict['files'])
+
+        if 'inputs' in test_dict:
+            test.text_inputs.extend(test_dict['inputs'])
+
+        tester.tests.append(test)
+
+    for result in tester.test():
+        print(result)
+
+    print(tester.result())
+
+main()
